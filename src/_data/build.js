7e4663f02ca526b92ba514e17432c2db05409e66
Marcos Martinez <contact@marcosmartinez.net>
2024-11-18 22:57:23 -0800

    Format files

--- src/_data/build.js
+++ src/_data/build.js
@@ -1,4 +1,4 @@
 export default {
     env: process.env.ELEVENTY_ENV,
-    timestamp: new Date()
-}
+    timestamp: new Date(),
+};
--- utils/filters.js
+++ utils/filters.js
@@ -1,28 +1,26 @@
-import { DateTime } from 'luxon'
+import { DateTime } from "luxon";
 
 export default {
     dateToFormat: function (date, format) {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toFormat(
-            String(format)
-        )
+        return DateTime.fromJSDate(date, { zone: "utc" }).toFormat(String(format));
     },
 
     dateToISO: function (date) {
-        return DateTime.fromJSDate(date, { zone: 'utc' }).toISO({
+        return DateTime.fromJSDate(date, { zone: "utc" }).toISO({
             includeOffset: false,
-            suppressMilliseconds: true
-        })
+            suppressMilliseconds: true,
+        });
     },
 
     obfuscate: function (str) {
-        const chars = []
+        const chars = [];
         for (var i = str.length - 1; i >= 0; i--) {
-            chars.unshift(['&#', str[i].charCodeAt(), ';'].join(''))
+            chars.unshift(["&#", str[i].charCodeAt(), ";"].join(""));
         }
-        return chars.join('')
+        return chars.join("");
     },
 
-    filterTagList (tags) {
-        return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-    }
-}
+    filterTagList(tags) {
+        return (tags || []).filter((tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+    },
+};
